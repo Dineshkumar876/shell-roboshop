32477a7c3b975850c35ad6897d6ab2e4be4079ae
Dinesh <dinesh@vasa.com>
2025-07-09 01:52:11 +0530

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -17,7 +17,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
 
  if [ $instance != "frontend" ]
  then
- IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivteIpAddress' --output text)
+ IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
  else
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
  fi
