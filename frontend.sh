aea9df72fba8d895ae0abdca77853b1acb7bc698
Dinesh <dinesh@vasa.com>
2025-07-09 12:42:55 +0530

    Hello world

--- frontend.sh
+++ frontend.sh
@@ -44,7 +44,7 @@ VALIDATE $? "Installing Nginx"
 systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enable Nginx"
 
-systemctl start nginx &>>$LOG_FILE
+systemctl start nginx 
 VALIDATE $? "Start Nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
