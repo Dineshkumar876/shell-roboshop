cb82bbbfc4c8acfac734a07cd21fcbf608bf1918
Dinesh <dinesh@vasa.com>
2025-07-09 17:15:22 +0530

    hello world

---
+++ shipping.sh
@@ -0,0 +1,86 @@
+ #!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install maven -y &>>$LOG_FILE
+VALIDATE $? "Installing Maven and Java"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+   VALIDATE $? "System Create User in roboshop"
+else
+   echo -e "System user roboshop already created ... $Y SKIPPING $N"
+   fi
+
+mkdir -p /app 
+VALIDATE $? "Create make directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Create Curl"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "Unzip File"
+
+mvn clean package &>>$LOG_FILE
+VALIDATE $? "Clean Package"
+
+mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+VALIDATE $? "Target Shipping Jar"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Deemon Reload"
+
+systemctl enable shipping &>>$LOG_FILE
+VALIDATE $? "Enable Shipping"
+
+systemctl start shipping &>>$LOG_FILE
+VALIDATE $? "Start Shipping"
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Install Mysql"
+
+mysql -h 172.31.26.124 -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+mysql -h 172.31.26.124 -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE 
+mysql -h 172.31.26.124 -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+VALIDATE $? "Loading Data in Mysql"
+
+systemctl restart shipping &>>$LOG_FILE
+VALIDATE $? "Restart Shipping"
+
+
