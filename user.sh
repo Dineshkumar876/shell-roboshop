d14431e34eceda420fec5e0e54feaa495993b602
Dinesh <dinesh@vasa.com>
2025-07-09 15:51:38 +0530

    hello world

--- user.sh
+++ user.sh
@@ -32,32 +32,32 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disable Nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enable Nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "$Y Installing Nodejs $N"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 VALIDATE $? "System user create"
 
-mkdir /app 
+mkdir /app &>>$LOG_FILE
 VALIDATE $? "Make Directory"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
 VALIDATE $? "Installing Server"
 
 cd /app 
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>$LOG_FILE
 VALIDATE $? "Unzip Roboshop"
 
-npm install
+npm install &>>$LOG_FILE
 VALIDATE $? "Install npm"
 
-cp $SCRIPT_DIR user.service /etc/systemd/system/user.service
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
 VALIDATE $? "Copy make Directory"
 
 systemctl daemon-reload
