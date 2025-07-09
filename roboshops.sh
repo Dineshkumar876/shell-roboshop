754e032b5b8e74b811890be2292ceb5638406920
Dinesh <dinesh@vasa.com>
2025-07-09 22:10:53 +0530

    hello world

--- roboshops.sh
+++ roboshops.sh
@@ -1,17 +1,13 @@
  #!/bin/bash
  
  AMI_ID="ami-09c813fb71547fc4f"
- SG_ID="sg-0b818e73a2211b60c"
+ SG_ID="sg-0ccf0fa1fb801a27e"
  INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
  ZONE_ID="Z0320932ZN57KIJC569U"
  DOMAIN_NAME="vasadinesh.site"
  for instance in ${INSTANCES[@]}
  do
-    INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-09c813fb71547fc4f \
-    --instance-type t2.micro \
-    --security-group-ids sg-0b818e73a2211b60c \
-    --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=test}]')
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0ccf0fa1fb801a27 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
     IP=$()
