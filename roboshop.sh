3db4d5054dd112da744f9e0a092a12e109411967
Dinesh <dinesh@vasa.com>
2025-07-09 07:59:42 +0530

    Hello world

--- roboshop.sh
+++ roboshop.sh
@@ -7,20 +7,13 @@
  DOMAIN_NAME="vasadinesh.site"
  for instance in ${INSTANCES[@]}
  do
-INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-09c813fb71547fc4f \
-    --instance-type t2.micro \
-    --security-group-ids sg-0ccf0fa1fb801a27e \
-    --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=$instance}]' \
-    --query 'Instances[0].InstanceId' \
-    --output text)
-
- if [ $instance != "frontend" ]
- then
- IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
- else
- IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
- fi
- echo "$instance IP address: $IP"
- done 
-
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+  if [ $instance != "frontend" ]
+  then
+  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+  RECORD_NAME="$instance.$DOMAIN_NAME"
+  else
+  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+  RECORD_NAME="$DOMAIN_NAME"
+  fi
+  
