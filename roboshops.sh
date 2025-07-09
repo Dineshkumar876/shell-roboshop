adc427160a582063e36b758fc7ebb2574eba87de
Dinesh <dinesh@vasa.com>
2025-07-09 12:14:12 +0530

    Hello world

--- roboshops.sh
+++ roboshops.sh
@@ -1,38 +1,24 @@
  #!/bin/bash
  
  AMI_ID="ami-09c813fb71547fc4f"
- SG_ID="sg-0ccf0fa1fb801a27e"
+ SG_ID="sg-0b818e73a2211b60c"
  INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
  ZONE_ID="Z0320932ZN57KIJC569U"
  DOMAIN_NAME="vasadinesh.site"
  for instance in ${INSTANCES[@]}
  do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0ccf0fa1fb801a27e --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id ami-09c813fb71547fc4f \
+    --instance-type t2.micro \
+    --security-group-ids sg-0b818e73a2211b60c \
+    --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=test}]')
     if [ $instance != "frontend" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+    IP=$()
+    RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }'
-done
+ done
